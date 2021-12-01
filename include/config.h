67f524513fa0ddb567d42877625882891f654a5e
ParkjooWon <ghonman2@ajou.ac.kr>
2021-12-01 22:19:22 +0900

    server modify M

--- include/config.h
+++ include/config.h
@@ -7,7 +7,7 @@
 #define CONFIG_SERVER_IP "10.10.1.17"
 #define CONFIG_SERVER_PORT 50000
 #define CONFIG_MULTI_IP "10.10.1.13"
-#define MY_ADDR "10.10.1.12"
+#define MY_ADDR "10.10.1.13"
 
 enum {
 	NR_RECVER = 1,
