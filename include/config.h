e7a4e6191dfb5caf3388a9d42372a0894c1ddf06
ParkJooWOn <ghonman2@ajou.ac.kr>
2021-12-28 13:38:15 +0900

    seave

--- include/config.h
+++ include/config.h
@@ -4,16 +4,16 @@
 #define __CONFIG_H__
 
 
-#define CONFIG_SERVER_IP "10.10.1.12"
+#define CONFIG_SERVER_IP "10.10.1.17"
 #define CONFIG_SERVER_PORT 50000
 #define CONFIG_MULTI_IP "10.10.1.13"
-#define MY_ADDR "10.10.1.17"
+#define MY_ADDR "10.10.1.12"
 
 enum {
 	NR_RECVER = 1,
 	NR_WORKER = 1,
 
-	PRE_ALLOC_MEMBLOCK = 30,
+	PRE_ALLOC_MEMBLOCK = 1,
 };
 
 #endif 
